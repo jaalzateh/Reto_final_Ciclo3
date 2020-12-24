bff2784822dcbc5feef820323fbe2d466b581403
caariasu <c-arias@outlook.com>
2020-12-24 01:06:04 +0100

    Menu final y ubicaciòon de carrousel en home

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import About from '../views/About.vue'
+// import About from '../views/About.vue'
+import Carousel from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'About',
-    component: About
+    name: 'Home',
+    component: Carousel
   },
   {
-    path: '/home',
-    name: 'home',
+    path: '/acerca',
+    name: 'Acerca',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
-    meta:{
-      requiresAuth: true
-    }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: '/admin',
