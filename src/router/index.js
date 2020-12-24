107edfca0532d2722dda3609a5293df582cdea9e
jaalzateh <jaalzateh@unal.edu.co>
2020-12-24 08:32:50 -0500

    Sección contacto lista

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,14 @@ const routes = [
       component: () => import(/* webpackChunkName: "about" */ '../views/CasoExito.vue'),
   },
   {
+    path: '/contacto',
+    name: 'Contacto',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contacto.vue'),
+},
+  {
     path: '/admin',
     name: 'Admin',
     // route level code-splitting
